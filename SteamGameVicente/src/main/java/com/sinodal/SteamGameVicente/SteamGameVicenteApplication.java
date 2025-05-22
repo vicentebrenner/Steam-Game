b2fb6889743206c072aab0d0e6f083c88226a44e
vicentebrenner <vicentebrenner10@gmail.com>
2025-05-22 19:55:51 -0300

    Update SteamGameVicenteApplication.java

--- SteamGameVicente/src/main/java/com/sinodal/SteamGameVicente/SteamGameVicenteApplication.java
+++ SteamGameVicente/src/main/java/com/sinodal/SteamGameVicente/SteamGameVicenteApplication.java
@@ -1,4 +1,4 @@
-package SteamGameVicente;
+package view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SteamGameVicenteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SteamGameVicenteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SteamGameVicenteApplication.class, args);
+    }
 }
