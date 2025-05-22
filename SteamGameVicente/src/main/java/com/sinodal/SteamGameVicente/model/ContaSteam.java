6d0e58335cab2974e16a359d24dfec4b20ba5bf9
vicentebrenner <vicentebrenner10@gmail.com>
2025-05-22 19:53:51 -0300

    Update ContaSteam.java

--- SteamGameVicente/src/main/java/com/sinodal/SteamGameVicente/model/ContaSteam.java
+++ SteamGameVicente/src/main/java/com/sinodal/SteamGameVicente/model/ContaSteam.java
@@ -1,22 +1,25 @@
+package model;
+
+import java.util.ArrayList;
 import java.util.List;
 
-class ContaSteam {
+public class ContaSteam {
     private String nomeConta;
     private List<Usuario> usuarios;
-    
+
     public ContaSteam(String nomeConta) {
         this.nomeConta = nomeConta;
-        this.usuarios = new java.util.ArrayList<>();
+        this.usuarios = new ArrayList<>();
     }
-    
+
     public String getNomeConta() {
         return nomeConta;
     }
-    
+
     public void adicionarUsuario(Usuario usuario) {
         usuarios.add(usuario);
     }
-    
+
     public List<Usuario> getUsuarios() {
         return usuarios;
     }
